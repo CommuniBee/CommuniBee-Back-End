e4959f29b4ce4f084052a533add717dca243c17a
Or Bin <orbin50@gmail.com>
2018-11-29 16:55:41 +0200

    Added semicolons to code lines

--- server.js
+++ server.js
@@ -1,49 +1,48 @@
-const Koa        = require('koa')
-const Router     = require('koa-router')
-const Logger     = require('koa-logger')
-const Cors       = require('@koa/cors')
-const BodyParser = require('koa-bodyparser')
-const Helmet     = require('koa-helmet')
-const respond    = require('koa-respond')
-const serve      = require('koa-static');
-const koaSwagger = require('koa2-swagger-ui')
+const Koa = require('koa');
+const Router = require('koa-router');
+const Logger = require('koa-logger');
+const Cors = require('@koa/cors');
+const BodyParser = require('koa-bodyparser');
+const Helmet = require('koa-helmet');
+const respond = require('koa-respond');
+const serve = require('koa-static');
+const koaSwagger = require('koa2-swagger-ui');
 
-const app = new Koa()
-const router = new Router()
+const app = new Koa();
+const router = new Router();
 
 app.use(
   koaSwagger({
     routePrefix: '/swagger', // host at /swagger instead of default /docs
     swaggerOptions: {
-      url: 'http://localhost:3000/api-specs.json', // example path to json
+      url: 'http://localhost:3000/api-specs.json' // example path to json
     },
   }),
 );
 
-
-app.use(Helmet())
+app.use(Helmet());
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(Logger())
+  app.use(Logger());
 }
 
-app.use(Cors())
+app.use(Cors());
 app.use(BodyParser({
   enableTypes: ['json'],
   jsonLimit: '5mb',
   strict: true,
   onerror: function (err, ctx) {
-    ctx.throw('body parse error', 422)
-  }
-}))
+    ctx.throw('body parse error', 422);
+  },
+}));
 
-app.use(respond())
+app.use(respond());
 
 app.use(serve(__dirname + '/public'));
 
 // API routes
-require('./routes')(router)
-app.use(router.routes())
-app.use(router.allowedMethods())
+require('./routes')(router);
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-module.exports = app
+module.exports = app;
