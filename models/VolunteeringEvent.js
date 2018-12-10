c2694e91ada3504bc7fd5924a3146d9853787ba0
AsafKaravani <asaf.karavani1@gmail.com>
2018-12-10 02:41:00 +0200

    added - VolunteeringEvent Model

--- models/VolunteeringEvent.js
+++ models/VolunteeringEvent.js
@@ -9,7 +9,27 @@ var Schema = mongoose.Schema;
  * VolunteeringEvent schema
  */
 var VolunteeringEventSchema = new Schema({
-
+  request: {
+    type: Schema.Types.ObjectId,
+    ref: 'VolunteeringRequest'
+  },
+  offer: {
+    type: Schema.Types.ObjectId,
+    ref: 'VolunteeringOffer'
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  userInitiated: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  files: {
+    type: [String],
+    default: []
+  }
 });
 
 /**
