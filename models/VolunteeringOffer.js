5490dbade98016e764442799b1d93597369813b1
AsafKaravani <asaf.karavani1@gmail.com>
2018-12-10 02:39:22 +0200

    added - VolunteeringOffer Model

--- models/VolunteeringOffer.js
+++ models/VolunteeringOffer.js
@@ -9,6 +9,61 @@ var Schema = mongoose.Schema;
  * VolunteeringOffer schema
  */
 var VolunteeringOfferSchema = new Schema({
+  location: {
+    type: {
+      lat: Number,
+      lon: Number
+    },
+    required: true
+  },
+  volunteersOffered: {
+    type: Number,
+    required: true
+  },
+  occurrences: {
+    type: Number,
+    required: true
+  },
+  contact: {
+    name:{
+      type: String,
+      required: true
+    },
+    phone:{
+      type: String,
+      required: true
+    },
+    email:{
+      type: String,
+      required: true
+    }
+  },
+  dateRange: {
+    type: {
+      from: Date,
+      to: Date
+    },
+    required: true
+  },
+  availableDays: {
+    type: [Number],
+    required: true
+  },
+  timeRange:  {
+    type: {
+      from: Date,
+      to: Date
+    },
+    required: true
+  },
+  notes: {
+    type: [String],
+    default: []
+  },
+  content: {
+    type: Schema.Types.ObjectId,
+    ref: 'Content'
+  }
 
 });
 
