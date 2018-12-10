1521985d4194c54bdf81bff41b09195926acde08
bsuro10 <36992518+bsuro10@users.noreply.github.com>
2018-12-10 17:28:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ server.listen(port, () => console.log(`API server started on ${port}`));
 
 // Database connection
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:PVVQUGQOAWYOZZIH@portal-ssl359-47.bmix-dal-yp-b709d2f7-d3d3-4bce-9a64-a3e5497e93f5.4290300913.composedb.com:17949,portal-ssl354-45.bmix-dal-yp-b709d2f7-d3d3-4bce-9a64-a3e5497e93f5.4290300913.composedb.com:17949/compose?authSource=admin&ssl=true');
+//mongoose.connect('mongodb://{connection_url}');
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
