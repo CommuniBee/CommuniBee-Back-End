cf8e3dd5ee003f3635ff1201b48b8b76a661af9e
Or Bin <orbin50@gmail.com>
2018-12-11 08:15:08 +0200

    Created routing and empty routers for each endpoint group

---
+++ routes/contents.js
@@ -0,0 +1,7 @@
+const Router = require('koa-router');
+
+const router = new Router();
+
+// Add sub-routes here using router.get(...), router.post(...), etc.
+
+module.exports = router.routes();
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
-const usersRoutes = require('./users');
+const categoriesRoutes = require('./categories');
+const contentsRoutes = require('./contents');
 const regionsRoutes = require('./regions');
+const usersRoutes = require('./users');
+const volunteeringEventsRoutes = require('./volunteering_events');
+const volunteeringOffersRoutes = require('./volunteering_offers');
+const volunteeringRequestsRoutes = require('./volunteering_requests');
 
 module.exports = (router) => {
   router.prefix('/v1');
-  router.use('/users', usersRoutes);
+  router.use('/categories', categoriesRoutes);
+  router.use('/contents', contentsRoutes);
   router.use('/regions', regionsRoutes);
+  router.use('/users', usersRoutes);
+  router.use('/volunteering_events', volunteeringEventsRoutes);
+  router.use('/volunteering_offers', volunteeringOffersRoutes);
+  router.use('/volunteering_requests', volunteeringRequestsRoutes);
 };
