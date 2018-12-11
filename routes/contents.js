489f34afca3fdd7d273552868bb5db9e65f78f2c
Ran Biderman <bsuro10@gmail.com>
2018-12-11 10:32:16 +0200

    Adding GET /contents

--- routes/contents.js
+++ routes/contents.js
@@ -1,7 +1,15 @@
 const Router = require('koa-router');
+const Content = require('../models/Content');
 
 const router = new Router();
 
-// Add sub-routes here using router.get(...), router.post(...), etc.
+router.get('/', async (ctx) => {
+    await Content.find({}, (err, doc) => {
+      if (err) {
+        ctx.internalServerError();
+      }
+      ctx.ok(doc);
+    });
+  });
 
 module.exports = router.routes();
