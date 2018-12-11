089dcfe1f388dff1f736530d2da67140a1ba51e8
Ran Biderman <bsuro10@gmail.com>
2018-12-11 13:03:56 +0200

    Adding create function to contents route

--- routes/contents.js
+++ routes/contents.js
@@ -3,13 +3,34 @@ const Content = require('../models/Content');
 
 const router = new Router();
 
-router.get('/', async (ctx) => {
+router.get('/', list)
+    .post('/', create);
+
+async function list(ctx) {
     await Content.find({}, (err, doc) => {
-      if (err) {
-        ctx.internalServerError();
-      }
-      ctx.ok(doc);
-    });
-  });
+        if (err) {
+            ctx.internalServerError();
+        }
+        ctx.ok(doc);
+    })
+}
+
+async function create(ctx) {
+    var content = new Content(ctx.request.body);
+
+    try {
+        await content.save({
+            validateBeforeSave: true,
+        });
+        ctx.ok(content);
+    } catch (err) {
+        console.error(err);
+        if(err.name == 'ValidationError') {
+            ctx.badRequest('invalid document')    
+        } else {
+            ctx.internalServerError();
+        }
+    }
+}
 
 module.exports = router.routes();
