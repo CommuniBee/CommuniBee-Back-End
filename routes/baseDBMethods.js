d8813325769b352de221a531890ab483f5aae231
Ran Biderman <bsuro10@gmail.com>
2018-12-11 18:27:55 +0200

    Eslint

--- routes/baseDBMethods.js
+++ routes/baseDBMethods.js
@@ -1,53 +1,53 @@
-async function list(ctx, model) {
-    try {
-        var docs = await model.find({});
-        ctx.ok(docs);
-    } catch (error) {
-        ctx.internalServerError();
-    }
+async function list(ctx, Model) {
+  try {
+    const docs = await Model.find({});
+    ctx.ok(docs);
+  } catch (error) {
+    ctx.internalServerError();
+  }
 }
 
-async function create(ctx, model) {
-    var document = new model(ctx.request.body);
+async function create(ctx, Model) {
+  const document = new Model(ctx.request.body);
 
-    try {
-        await document.save({
-            validateBeforeSave: false,
-        });
-        ctx.ok(document);
-    } catch (err) {
-        console.error(err);
-        if(err.name == 'ValidationError') {
-            ctx.badRequest('invalid document')    
-        } else {
-            ctx.internalServerError();
-        }
+  try {
+    await document.save({
+      validateBeforeSave: false,
+    });
+    ctx.ok(document);
+  } catch (err) {
+    console.error(err);
+    if (err.name === 'ValidationError') {
+      ctx.badRequest('invalid document');
+    } else {
+      ctx.internalServerError();
     }
+  }
 }
 
-async function update(ctx, model) {
-    try {
-        var updatedDoc = await model.findOneAndUpdate({'_id': ctx.request.body._id }, 
-                                                      { $set: ctx.request.body }, 
-                                                      { new: true });
-        ctx.ok(updatedDoc);
-    } catch (error) {
-        ctx.internalServerError();
-    }
+async function update(ctx, Model) {
+  try {
+    const updatedDoc = await Model.findOneAndUpdate({ _id: ctx.request.body._id },
+      { $set: ctx.request.body },
+      { new: true });
+    ctx.ok(updatedDoc);
+  } catch (error) {
+    ctx.internalServerError();
+  }
 }
 
-async function remove(ctx, model) {
-    try {
-        var removedDoc = await model.findOneAndDelete({'_id': ctx.request.body._id });
-        ctx.ok(removedDoc);
-    } catch (error) {
-        ctx.internalServerError();
-    }
+async function remove(ctx, Model) {
+  try {
+    const removedDoc = await Model.findOneAndDelete({ _id: ctx.request.body._id });
+    ctx.ok(removedDoc);
+  } catch (error) {
+    ctx.internalServerError();
+  }
 }
 
 module.exports = {
-    list,
-    create,
-    update,
-    remove,
-}
+  list,
+  create,
+  update,
+  remove,
+};
--- routes/contents.js
+++ routes/contents.js
@@ -1,20 +1,20 @@
 const Router = require('koa-router');
 const Content = require('../models/Content');
-const DBMethods = require('./baseDBMethods')
+const DBMethods = require('./baseDBMethods');
 
 const router = new Router();
 
 router.get('/', async (ctx) => {
-        await DBMethods.list(ctx, Content);   
-    })
-    .post('/', async (ctx) => {
-        await DBMethods.create(ctx, Content);
-    })
-    .put('/', async (ctx) => {
-        await DBMethods.update(ctx, Content);
-    })
-    .delete('/', async (ctx) => {
-        await DBMethods.remove(ctx, Content);
-    });
+  await DBMethods.list(ctx, Content);
+})
+  .post('/', async (ctx) => {
+    await DBMethods.create(ctx, Content);
+  })
+  .put('/', async (ctx) => {
+    await DBMethods.update(ctx, Content);
+  })
+  .delete('/', async (ctx) => {
+    await DBMethods.remove(ctx, Content);
+  });
 
 module.exports = router.routes();
