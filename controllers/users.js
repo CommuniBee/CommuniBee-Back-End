72e07cf0a315291483e075ccf5ef5d90c6778855
Or Bin <orbin50@gmail.com>
2018-12-16 11:05:22 +0200

    Removed users handling in the back end

--- controllers/users.js
+++
@@ -1,9 +0,0 @@
-
-function hello(ctx) {
-  const user = ctx.request.query.user;
-  ctx.ok({ user });
-}
-
-module.exports = {
-  hello,
-};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const categoriesRoutes = require('./categories');
 const contentsRoutes = require('./contents');
 const regionsRoutes = require('./regions');
-const usersRoutes = require('./users');
 const volunteeringEventsRoutes = require('./volunteering_events');
 const volunteeringOffersRoutes = require('./volunteering_offers');
 const volunteeringRequestsRoutes = require('./volunteering_requests');
@@ -10,7 +9,6 @@ module.exports = (router) => {
   router.use('/categories', categoriesRoutes);
   router.use('/contents', contentsRoutes);
   router.use('/regions', regionsRoutes);
-  router.use('/users', usersRoutes);
   router.use('/volunteering-events', volunteeringEventsRoutes);
   router.use('/volunteering-offers', volunteeringOffersRoutes);
   router.use('/volunteering-requests', volunteeringRequestsRoutes);
